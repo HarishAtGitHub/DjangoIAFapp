2e9b6cfc764a7f350bd8773dfe18e83a799a07cc
Harish Kayarohanam <harish@collab.net>
2014-08-27 14:29:37 +0530

    Added the packaged libraries to system path

--- ctfbinaryappintegration/ctfbinaryappintegration/manage.py
+++ ctfbinaryappintegration/ctfbinaryappintegration/manage.py
@@ -5,7 +5,10 @@ from os.path import abspath, dirname
 
 if __name__ == "__main__":
     sys.path.insert(0, dirname(dirname(abspath(__file__))))
-    sys.path.append(dirname(abspath(__file__)) + '/lib/python2.6/site-packages')
+    sys.path.append(dirname(abspath(__file__)) + '/iaf_endpoint/webservice/rest/lib/python2.6/site-packages')
+    sys.path.append(dirname(abspath(__file__)) + '/iaf_endpoint/webservice/rest/lib/python2.6/site-packages/SOAPpy-0.12.5-py2.6.egg')
+    sys.path.append(dirname(abspath(__file__)) + '/iaf_endpoint/webservice/rest/lib/python2.6/site-packages/fpconst-0.7.2-py2.6.egg')
+    sys.path.append(dirname(abspath(__file__)) + '/iaf_endpoint/webservice/rest/lib/python2.6/site-packages/wstools-0.4-py2.6.egg')
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
